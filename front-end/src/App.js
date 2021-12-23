d88404fbf2e048a93092b8a74fdb7fd7991023c6
LAPTOP-1A660BBN\HP <darshantakarkhede5@gmail.com>
2021-12-23 14:30:43 +0530

    added static table for individual orders

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,254 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { BiEditAlt } from "react-icons/bi";
+
+ function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Order #</th>
+            <th scope="col">Order Date</th>
+            <th scope="col">Client</th>
+            <th scope="col">Location</th>
+            <th scope="col">Office</th>
+            <th scope="col">Region</th>
+            <th scope="col">Route</th>
+            <th scope="col">Delivery Type</th>
+            <th scope="col">Created By</th>
+            <th scope="col">Delivery Date</th>
+            <th scope="col">Trip Id</th>
+            <th scope="col">Delivery</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>OV1202503</td>
+            <td>2021-02-24</td>
+            <td>PP_Pollich-Nikolaus</td>
+            <td>South Fredy</td>
+            <td>Vancouver</td>
+            <td>Outside GVRD</td>
+            <td>test vancouver Van City route New Route</td>
+            <td></td>
+            <td>System User</td>
+            <td>2021-02-24</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button className="btn btn-danger"> Create Delivery</button>
+            </td>
+            <td>
+              <BiEditAlt />
+            </td>
+          </tr>
+          <tr>
+            <td>OV1202503</td>
+            <td>2021-02-24</td>
+            <td>PP_Pollich-Nikolaus</td>
+            <td>South Fredy</td>
+            <td>Vancouver</td>
+            <td>Outside GVRD</td>
+            <td>test vancouver Van City route New Route</td>
+            <td></td>
+            <td>System User</td>
+            <td>2021-02-24</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button className="btn btn-danger"> Create Delivery</button>
+            </td>
+            <td>
+              <BiEditAlt />
+            </td>
+          </tr>
+          <tr>
+            <td>OV1202503</td>
+            <td>2021-02-24</td>
+            <td>PP_Pollich-Nikolaus</td>
+            <td>South Fredy</td>
+            <td>Vancouver</td>
+            <td>Outside GVRD</td>
+            <td>test vancouver Van City route New Route</td>
+            <td></td>
+            <td>System User</td>
+            <td>2021-02-24</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button className="btn btn-danger"> Create Delivery</button>
+            </td>
+            <td>
+              <BiEditAlt />
+            </td>
+          </tr>
+          <tr>
+            <td>OV1202503</td>
+            <td>2021-02-24</td>
+            <td>PP_Pollich-Nikolaus</td>
+            <td>South Fredy</td>
+            <td>Vancouver</td>
+            <td>Outside GVRD</td>
+            <td>test vancouver Van City route New Route</td>
+            <td></td>
+            <td>System User</td>
+            <td>2021-02-24</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button className="btn btn-danger"> Create Delivery</button>
+            </td>
+            <td>
+              <BiEditAlt />
+            </td>
+          </tr>
+          <tr>
+            <td>OV1202503</td>
+            <td>2021-02-24</td>
+            <td>PP_Pollich-Nikolaus</td>
+            <td>South Fredy</td>
+            <td>Vancouver</td>
+            <td>Outside GVRD</td>
+            <td>test vancouver Van City route New Route</td>
+            <td></td>
+            <td>System User</td>
+            <td>2021-02-24</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button className="btn btn-danger"> Create Delivery</button>
+            </td>
+            <td>
+              <BiEditAlt />
+            </td>
+          </tr>
+          <tr>
+            <td>OV1202503</td>
+            <td>2021-02-24</td>
+            <td>PP_Pollich-Nikolaus</td>
+            <td>South Fredy</td>
+            <td>Vancouver</td>
+            <td>Outside GVRD</td>
+            <td>test vancouver Van City route New Route</td>
+            <td></td>
+            <td>System User</td>
+            <td>2021-02-24</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button className="btn btn-danger"> Create Delivery</button>
+            </td>
+            <td>
+              <BiEditAlt />
+            </td>
+          </tr>
+          <tr>
+            <td>OV1202503</td>
+            <td>2021-02-24</td>
+            <td>PP_Pollich-Nikolaus</td>
+            <td>South Fredy</td>
+            <td>Vancouver</td>
+            <td>Outside GVRD</td>
+            <td>test vancouver Van City route New Route</td>
+            <td></td>
+            <td>System User</td>
+            <td>2021-02-24</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button className="btn btn-danger"> Create Delivery</button>
+            </td>
+            <td>
+              <BiEditAlt />
+            </td>
+          </tr>
+          <tr>
+            <td>OV1202503</td>
+            <td>2021-02-24</td>
+            <td>PP_Pollich-Nikolaus</td>
+            <td>South Fredy</td>
+            <td>Vancouver</td>
+            <td>Outside GVRD</td>
+            <td>test vancouver Van City route New Route</td>
+            <td></td>
+            <td>System User</td>
+            <td>2021-02-24</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button className="btn btn-danger"> Create Delivery</button>
+            </td>
+            <td>
+              <BiEditAlt />
+            </td>
+          </tr>
+          <tr>
+            <td>OV1202503</td>
+            <td>2021-02-24</td>
+            <td>PP_Pollich-Nikolaus</td>
+            <td>South Fredy</td>
+            <td>Vancouver</td>
+            <td>Outside GVRD</td>
+            <td>test vancouver Van City route New Route</td>
+            <td></td>
+            <td>System User</td>
+            <td>2021-02-24</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button className="btn btn-danger"> Create Delivery</button>
+            </td>
+            <td>
+              <BiEditAlt />
+            </td>
+          </tr>
+          <tr>
+            <td>OV1202503</td>
+            <td>2021-02-24</td>
+            <td>PP_Pollich-Nikolaus</td>
+            <td>South Fredy</td>
+            <td>Vancouver</td>
+            <td>Outside GVRD</td>
+            <td>test vancouver Van City route New Route</td>
+            <td></td>
+            <td>System User</td>
+            <td>2021-02-24</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button className="btn btn-danger"> Create Delivery</button>
+            </td>
+            <td>
+              <BiEditAlt />
+            </td>
+          </tr>
+          <tr>
+            <td>OV1202503</td>
+            <td>2021-02-24</td>
+            <td>PP_Pollich-Nikolaus</td>
+            <td>South Fredy</td>
+            <td>Vancouver</td>
+            <td>Outside GVRD</td>
+            <td>test vancouver Van City route New Route</td>
+            <td></td>
+            <td>System User</td>
+            <td>2021-02-24</td>
+            <td>-</td>
+            <td>-</td>
+            <td>
+              <button className="btn btn-danger"> Create Delivery</button>
+            </td>
+            <td>
+              <BiEditAlt />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
+
 
 export default App;
